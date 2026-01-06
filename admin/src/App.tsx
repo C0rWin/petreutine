fb50a4f4a612a92811d01b3f56284c121d564259
Artem Barger <artem@bargr.net>
2026-01-06 23:47:05 +0200

    fix: Configure admin app base path for /admin deployment
    - Set Vite base to /admin/ for correct asset URLs
    - Set BrowserRouter basename to /admin for correct routing
    
    Signed-off-by: Artem Barger <artem@bargr.net>

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -57,7 +57,7 @@ function AppRoutes() {
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/admin">
       <AuthProvider>
         <AppRoutes />
       </AuthProvider>
